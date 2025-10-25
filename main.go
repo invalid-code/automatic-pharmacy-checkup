996cd35d7b26d7fa9d8fa420f42fee9b19d659f2
invalid-code <jessgaming55@gmail.com>
2025-10-25 20:46:23 +0800

    using headless browser

--- main.go
+++ main.go
@@ -1,40 +1,44 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
 	"os"
-	"strings"
+	"time"
 
+	"github.com/chromedp/chromedp"
 	"github.com/joho/godotenv"
 )
 
-const (
-	PAGE_ID     = 0
-	PSID    int = 0
-)
-
 func main() {
-	godotenv.Load()
-	reqBody := strings.NewReader(`
-{
-	"messaging_type": "RESPONSE",
-	"recipient": {
-		"id": ""
-	},
-	"message": {
-		"text": "hello"
+	if err := godotenv.Load(); err != nil {
+		panic("No .env file found")
 	}
-}
-`)
-	resp, err := http.Post(fmt.Sprintf("https://graph.facebook.com/v22.0/me/messages?access_token=%v", os.Getenv("ACCESS_TOKEN")), "application/json", reqBody)
+	msgr_acct := os.Getenv("MESSENGER_ACCT")
+	msgr_pass := os.Getenv("MESSENGER_PASS")
+	// opts := append(
+	// 	chromedp.DefaultExecAllocatorOptions[:],
+	// 	chromedp.Flag("no-sandbox", true),
+	// 	chromedp.Flag("headless", true),
+	// )
+	// allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	// defer cancel()
+
+	ctx, cancle := chromedp.NewContext(context.Background())
+	defer cancle()
+
+	var htmlContent string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate("https://www.messenger.com/"),
+		chromedp.Sleep(2*time.Second),
+		chromedp.SendKeys("#email", msgr_acct),
+		chromedp.SendKeys("#pass", msgr_pass),
+		chromedp.Click("#loginbutton"),
+		chromedp.Sleep(2*time.Second),
+		chromedp.OuterHTML("html", &htmlContent),
+	)
 	if err != nil {
 		panic(err)
 	}
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusOK {
-		fmt.Println("sent")
-	} else {
-		fmt.Println("failed to send")
-	}
+	fmt.Println(htmlContent)
 }
